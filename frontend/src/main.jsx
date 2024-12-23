8c78540b808af9f05ed7840f88d22374f6273498
eodgeorge <thinkeod@gmail.com>
2024-12-23 15:35:43 +0000

    mern-stack

---
+++ frontend/src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import App from './App.jsx';
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <BrowserRouter>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </BrowserRouter>
+  </React.StrictMode>,
+)
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App.jsx';
+// import { ChakraProvider } from '@chakra-ui/react';
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <ChakraProvider>
+//       <App />
+//     </ChakraProvider>
+//   </React.StrictMode>,
+// )
